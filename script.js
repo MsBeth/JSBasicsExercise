c7e39050c4bafd3218bd542f12c6c1773311dd3c
Maribeth Calipo <maribethsc43@gmail.com>
2021-09-21 17:32:18 -0400

    Created script.js file.

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log(18);
+
+// Numbers, Strings, Booleans, Null, Undefined
+
+// Addition is +, Subtraction is -, Multiplication *, Division is /, Exponentiation is **, Modulus is %
+
+console.log(5%4);
+console.log(5%2);
+
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+console.log("Hawk" + "eye");
+
+console.log("Maribeth".length + "Calipo".length);
+
+console.log("Avenger: Endgame"[16]);
+
+
+
+
+
+
